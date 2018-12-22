d6a95d87f732f0096e1e305844777b5a82ddee14
avinash.i.singh <avinash.i.singh@capgemini.com>
2018-12-22 16:34:57 +0000

    Added Elb settings

---
+++ elb.tf
@@ -0,0 +1,38 @@
+resource "aws_elb" "web" {
+  name = "hello-elb"
+
+  # The same availability zone as our instance
+  subnets = ["${aws_subnet.tf_test_subnet.id}"]
+
+  security_groups = ["${aws_security_group.elb.id}"]
+
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    target              = "HTTP:80/"
+    interval            = 30
+  }
+
+  # The instance is registered automatically
+
+  instances                   = ["${aws_instance.web.*.id}"]
+  cross_zone_load_balancing   = true
+  idle_timeout                = 400
+  connection_draining         = true
+  connection_draining_timeout = 400
+}
+
+resource "aws_lb_cookie_stickiness_policy" "default" {
+  name                     = "lbpolicy"
+  load_balancer            = "${aws_elb.web.id}"
+  lb_port                  = 80
+  cookie_expiration_period = 600
+}
